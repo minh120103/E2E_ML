d6e8217a0a9d474b7eb950366e26f63476aed24d
minh120103 <minh120103@gmail.com>
2025-07-14 16:17:16 +0700

    change port

--- main.py
+++ main.py
@@ -36,4 +36,5 @@ async def root():
     return {"message": "Welcome to the Churn Prediction API. Use /docs to view the API documentation."}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
+    # uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True) #AWS
